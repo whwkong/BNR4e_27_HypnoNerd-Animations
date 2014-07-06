a390f60a6364e01721743f4cbdcf3bd691673fb5
William Kong <riverw09@gmail.com>
2014-07-06 13:02:09 -0400

    - added alpha fade-in for message label - added "exploding" key-frame animation for message label

--- HypnoNerd/HypnoNerd/BNRHypnosisViewController.m
+++ HypnoNerd/HypnoNerd/BNRHypnosisViewController.m
@@ -17,6 +17,7 @@
 
 @implementation BNRHypnosisViewController
 
+#pragma mark - Controller life cycle
 -(instancetype) initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -37,6 +38,7 @@
     return self;
 }
 
+#pragma mark -
 -(void) loadView
 {
     NSLog(@"HVC loadView");
@@ -44,25 +46,51 @@
     // Create a view
     BNRHypnosisView *hypnosisView = [[BNRHypnosisView alloc] init];
     
-        // Add a textField
-        CGRect textFieldRect = CGRectMake(40,70,240,30);
-        UITextField *textField = [[UITextField alloc] initWithFrame:textFieldRect];
+    // Add a textField
+    CGRect textFieldRect = CGRectMake(40,70,240,30);
+    UITextField *textField = [[UITextField alloc] initWithFrame:textFieldRect];
     
-        // Setting the border style on the text field will allow us to see it more easily
-        textField.borderStyle = UITextBorderStyleRoundedRect;
+    // Setting the border style on the text field will allow us to see it more easily
+    textField.borderStyle = UITextBorderStyleRoundedRect;
     
-        textField.placeholder = @"Hypnotize me";
-        textField.returnKeyType = UIReturnKeyDone;
+    textField.placeholder = @"Hypnotize me";
+    textField.returnKeyType = UIReturnKeyDone;
     
-        textField.keyboardType = UIKeyboardTypeASCIICapable;
+    textField.keyboardType = UIKeyboardTypeASCIICapable;
     
-        [hypnosisView addSubview:textField];
+    [hypnosisView addSubview:textField];
     
-        textField.delegate = self;
+    textField.delegate = self;
     
     self.view = hypnosisView;
 }
 
+#pragma mark - View life cycle
+-(void) viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    NSLog(@"HVC viewWillAppear:");
+}
+
+-(void) viewDidLoad
+{
+    [super viewDidLoad];
+    
+    NSLog(@"HVC viewDidLoad");
+}
+
+#pragma mark - Actions
+- (BOOL)textFieldShouldReturn:(UITextField *) textField
+{
+    [self drawHypnoticMessage:textField.text];
+    
+    textField.text = @"";
+    [textField resignFirstResponder];
+    
+    return YES;
+}
+
+#pragma mark - Drawing method
 // Creates 20 UILabels on the screen at random positions
 -(void) drawHypnoticMessage:(NSString*) message
 {
@@ -93,6 +121,39 @@
         // Add the label to hierarchy
         [self.view addSubview:messageLabel];
         
+        // Animation effects
+        // Set label's initial alpha
+        messageLabel.alpha = 0.0;
+        
+        // Animate the alpha to 1.0/
+        [UIView animateWithDuration:0.5
+                              delay:0.0
+                            options:UIViewAnimationOptionCurveEaseInOut
+                         animations:^{
+                             messageLabel.alpha = 1.0;
+                         }
+                         completion:NULL];
+        
+        [UIView animateKeyframesWithDuration:1.0
+               delay:0.0
+             options:0
+          animations:^{
+              [UIView addKeyframeWithRelativeStartTime:0
+                  relativeDuration:0.8
+                        animations:^{
+                            messageLabel.center = self.view.center;}];
+              
+              [UIView addKeyframeWithRelativeStartTime:0.8
+                  relativeDuration:0.2
+                        animations:^ {
+                            int x = arc4random()%width;
+                            int y = arc4random()%height;
+                            messageLabel.center = CGPointMake(x,y);
+                        }];
+            }
+          completion:NULL];
+       
+        
         // Add motion effects for iOS 7 devices
         UIInterpolatingMotionEffect *motionEffect;
         motionEffect = [[UIInterpolatingMotionEffect alloc] initWithKeyPath:@"center.x"
@@ -107,31 +168,6 @@
         motionEffect.maximumRelativeValue = @(25);
         [messageLabel addMotionEffect:motionEffect];
     }
-}
-
-
--(void) viewWillAppear:(BOOL)animated
-{
-    [super viewWillAppear:animated];
-    NSLog(@"HVC viewWillAppear:");
-}
-
--(void) viewDidLoad
-{
-    [super viewDidLoad];
-    
-    NSLog(@"HVC viewDidLoad");
-}
-
-
-- (BOOL)textFieldShouldReturn:(UITextField *) textField
-{
-    [self drawHypnoticMessage:textField.text];
-    
-    textField.text = @"";
-    [textField resignFirstResponder];
-    
-    return YES;
 }
 
 
--- README.md
+++ README.md
@@ -1,13 +1,23 @@
-BNR4e_Ch07_HypnoNerd
+BNR4e_Ch27_ControllingAnimations-HypnoNerd
 ====================
 
 Big Nerd Ranch's iOS Programming 4th ed, Hillegass & Conway
-Ch7-Delegation and Text Input
-HypnoNerd app 
-The purpose of this chapter :
-⁃ introduces text fields : UITextField 
-⁃ introduces First Responder concept : UIFirstResponder
-⁃ introduces the Delegate design pattern 
-⁃ introduces Protocols (similar to Java's Interface) 
-- introduces motion effects for iOS 7 devices
+Ch27-Controlling Animations
 
+HypnoNerd app 
+- this app builds on the functionality of the HypnoNerd app from chapter 7.
+
+Chapter 27 covers the following :
+- various animation techniques
+	- alpha fade in
+	- timing functions
+	- keyframe animation
+
+
+-----------------------------------
+Tagging Conventions: 
+
+Commits that correspond to the completion of a chapter are tagged 0.4.x, 
+where x is the chapter number, and 4 represents the 4th edition of BNR. 
+
+-----------------------------------
